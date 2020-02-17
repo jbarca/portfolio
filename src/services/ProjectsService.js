8bef8125aeef16139fd95072a16d9c7dbcd362ff
Jacob Barca <jacob.barca1@hotmail.com>
2020-02-17 19:14:46 +1100

    set up airtable backend with app

---
+++ src/services/ProjectsService.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const Axios = axios.create({
+    baseURL: "https://api.airtable.com/v0/app5AvJLIQc8kl3qm/Table%201"
+})
+
+Axios.defaults.headers.common = {'Authorization': `Bearer ` + process.env.VUE_APP_AIRTABLEKEY}
+
+export default {
+    getProjects() {
+        return Axios.get()
+    }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-
+        <div class="hero is-cover is-relative is-fullheight-with-navbar is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">Hello, I'm Jacob.</h1>
+                    <h2 class="subtitle is-3">A customer focused, entrepreneurially minded
+                        web developer.</h2>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
